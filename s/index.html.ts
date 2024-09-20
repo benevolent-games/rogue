b004498446687590589073be7328eb005b0a5f3d
Chase Moskal <chasemoskal@gmail.com>
2024-09-20 03:11:03 -0700

    wip: new startup

--- s/index.html.ts
+++ s/index.html.ts
@@ -1,9 +1,11 @@
 
 import "@benev/slate/x/node.js"
-import {template, html, easypage, startup_scripts_with_dev_mode, git_commit_hash} from "@benev/turtle"
+import {template, html, easypage, startup_scripts_with_dev_mode, git_commit_hash, read_file, unsanitized} from "@benev/turtle"
 
 export default template(async basic => {
 	const path = basic.path(import.meta.url)
+	const hash = await git_commit_hash()
+	const shorthash = hash.slice(0, 6)
 
 	return easypage({
 		path,
@@ -13,7 +15,7 @@ export default template(async basic => {
 		head: html`
 			<link rel="icon" href="/assets/graphics/favicon.png"/>
 			<link rel="stylesheet" href="${path.version.root("index.css")}"/>
-			<meta data-commit-hash="${await git_commit_hash()}"/>
+			<meta data-commit-hash="${hash}"/>
 
 			<link rel="preconnect" href="https://fonts.googleapis.com">
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -22,14 +24,11 @@ export default template(async basic => {
 			<link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
 			<link href="https://fonts.googleapis.com/css2?family=Forum&family=Suez+One&family=Uncial+Antiqua&display=swap" rel="stylesheet">
 
-			${startup_scripts_with_dev_mode({
-				path,
-				scripts: [{
-					module: "index.bundle.js",
-					bundle: "index.bundle.min.js",
-					hash: false,
-				}],
-			})}
+			<script type="importmap">
+				${unsanitized(await read_file("x/importmap.json"))}
+			</script>
+
+			<script type="module" src="/index.bundle.js"></script>
 		`,
 		body: html`
 			<game-app></game-app>
@@ -37,3 +36,12 @@ export default template(async basic => {
 	})
 })
 
+			// ${startup_scripts_with_dev_mode({
+			// 	path,
+			// 	es_module_shims: null,
+			// 	scripts: [{
+			// 		module: "index.bundle.js",
+			// 		bundle: "index.bundle.min.js",
+			// 		hash: false,
+			// 	}],
+			// })}
