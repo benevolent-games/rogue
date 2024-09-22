e871b572d4d34cbc3a3726feea0826c4862fc81b
Chase Moskal <chasemoskal@gmail.com>
2024-09-22 16:52:09 -0700

    code: inbox/outbox adaptive buffer

---
+++ s/logic/framework/relay/inbox-outbox.ts
@@ -0,0 +1,96 @@
+
+import {RunningAverage} from "@benev/toolbox"
+
+export type ParcelId = number
+export type ParcelTime = number
+export type Parcel<P> = [ParcelId, ParcelTime, P]
+
+export class Outbox<P> {
+	id = 0
+	start: number
+
+	constructor(private now = () => Date.now()) {
+		this.start = now()
+	}
+
+	wrap(payload: P): Parcel<P> {
+		const id = this.id++
+		const time = this.now() - this.start
+		return [id, time, payload]
+	}
+}
+
+export class Inbox<P> {
+	#start: number
+	#offsets: RunningAverage
+	#buffer = new Map<ParcelId, Parcel<P>>
+	#nanny = new Nanny()
+
+	constructor(
+			public delay = 100,
+			public smoothing = 20,
+			private now = () => Date.now(),
+		) {
+		this.#start = now()
+		this.#offsets = new RunningAverage(smoothing)
+	}
+
+	ingest(parcel: Parcel<P>) {
+		const [id, time] = parcel
+		if (this.#buffer.has(id)) return undefined
+		this.#buffer.set(id, parcel)
+		this.#offsets.add(this.#offset(time))
+	}
+
+	take(): P[] {
+		const ready: Parcel<P>[] = []
+		const localtime = this.#localtime
+
+		for (const parcel of this.#buffer.values()) {
+			const [id, time] = parcel
+
+			const offset = this.#offset(time, localtime)
+			const abberation = offset - this.#offsets.average
+			const correctedTime = (time + offset) - abberation
+			const since = localtime - correctedTime
+
+			if (since >= this.delay) {
+				ready.push(parcel)
+				this.#buffer.delete(id)
+			}
+		}
+
+		return ready
+			.sort(sortById)
+			.filter(this.#nanny.removeDisorderly)
+			.map(getPayload)
+	}
+
+	get #localtime() {
+		return this.now() - this.#start
+	}
+
+	#offset(time: number, localtime = this.#localtime) {
+		return localtime - time
+	}
+}
+
+class Nanny {
+	biggest: number = -1
+
+	removeDisorderly = ([id]: Parcel<any>) => {
+		if (id <= this.biggest)
+			return false
+		this.biggest = id
+		return true
+	}
+}
+
+function getPayload<P>([,,payload]: Parcel<P>): P {
+	return payload
+}
+
+function sortById([idA]: Parcel<any>, [idB]: Parcel<any>) {
+	return idA - idB
+}
+
