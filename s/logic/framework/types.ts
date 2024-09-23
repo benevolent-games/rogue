98e776d48743f9acb0eeaa19d74072be94376d53
Chase Moskal <chasemoskal@gmail.com>
2024-09-22 18:13:10 -0700

    tidy: remove obsolete code

--- s/logic/framework/types.ts
+++ s/logic/framework/types.ts
@@ -19,21 +19,3 @@ export type Archetype = {
 	broadcast: Broadcast
 }
 
-// export namespace FeedEvent {
-// 	export type Create = {kind: "create", entityId: number, state: State}
-// 	export type Facts = {kind: "facts", entityId: number, facts: any}
-// 	export type Broadcast = {kind: "broadcast", entityId: number, broadcast: any}
-// 	export type Destroy = {kind: "destroy", entityId: number}
-//
-// 	export type Any = Create | Facts | Broadcast | Destroy
-// 	export type Kind = Any["kind"]
-// }
-//
-// export namespace FeedbackEvent {
-// 	export type Data = {kind: "data", entityId: number, data: any}
-// 	export type Memo = {kind: "memo", entityId: number, memo: any}
-//
-// 	export type Any = Data | Memo
-// 	export type Kind = Any["kind"]
-// }
-
