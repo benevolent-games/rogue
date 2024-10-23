ae20ca610b1b63881290bad7d10e0582e6dc680a
Chase Moskal <chasemoskal@gmail.com>
2024-10-23 12:18:21 -0700

    add: user icons

---
+++ s/dom/icons/tabler/user-off.svg.ts
@@ -0,0 +1,12 @@
+
+import {svg} from "@benev/slate"
+
+export default svg`
+	<svg xmlns="http://www.w3.org/2000/svg" x-bind:width="size" x-bind:height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" x-bind:stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2" style="--darkreader-inline-stroke: currentColor;" data-darkreader-inline-stroke="">
+		<path d="M8.18 8.189a4.01 4.01 0 0 0 2.616 2.627m3.507 -.545a4 4 0 1 0 -5.59 -5.552"></path>
+		<path d="M6 21v-2a4 4 0 0 1 4 -4h4c.412 0 .81 .062 1.183 .178m2.633 2.618c.12 .38 .184 .785 .184 1.204v2"></path>
+		<path d="M3 3l18 18"></path>
+	</svg>
+`
+
+
