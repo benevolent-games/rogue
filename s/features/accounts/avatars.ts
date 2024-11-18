ae239b95eb8e8a97827708826476ba61ee37ae58
Chase Moskal <chasemoskal@gmail.com>
2024-11-18 12:34:18 -0800

    add: new avatars for anons

--- s/features/accounts/avatars.ts
+++ s/features/accounts/avatars.ts
@@ -1,10 +1,10 @@
 
 import {Map2} from "@benev/slate"
 
-export type AvatarKind = "free" | "premium" | "rare"
+export type AvatarKind = "anon" | "free" | "premium" | "rare"
 
 export class Avatar {
-	constructor(public id: string, public kind: "free" | "premium" | "rare") {}
+	constructor(public id: string, public kind: AvatarKind) {}
 
 	get url() {
 		return `/assets/avatars/${this.id}.webp`
@@ -14,11 +14,16 @@ export class Avatar {
 		return ids.map(id => [id, new this(id, kind)] as [string, Avatar])
 	}
 
-
-
-
-
 	static library = new Map2<string, Avatar>([
+		...this.#group("anon", [
+			"64tcJ1tTCF7",
+			"499UCh29USv",
+			"Kz7fjUWdShz",
+			"Y6xnbNkvJCg",
+			"f2TMdv7Jabe",
+			"h6DEGDRTteC",
+		]),
+
 		...this.#group("free", [
 			"cLZcTnPDhh8",
 			"M1xwCjPJq5s",
