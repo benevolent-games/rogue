78c2c9be68856e95ca942a752bffebcdadd102e0
Chase Moskal <chasemoskal@gmail.com>
2024-12-02 22:51:54 -0800

    fix: end yendor rooms

--- s/logic/dungeons/layouting/algos/yendor.ts
+++ s/logic/dungeons/layouting/algos/yendor.ts
@@ -4,15 +4,18 @@ import {Fattener} from "./utils/fattener.js"
 import {goalposting} from "./utils/goalposting.js"
 
 export const yendor = cellAlgo(options => {
+	const middleTile = options.tileGrid.extent.clone().divideBy(2).round()
+
 	const {walkables, goalposts} = goalposting({
 		...options,
+		end: middleTile,
 		goalcountRange: [0, 0],
 		distanceAlgo: "manhattan",
 	})
 	const fattener = new Fattener(walkables, options)
 
 	fattener.shadow()
-	fattener.spawnRectangle(options.end, [5, 10], true)
+	fattener.spawnRectangle(middleTile, [5, 10], true)
 	fattener.makeBorderRooms({sizeRange: [2, 3]})
 
 	return {walkables, goalposts, spawnpoints: []}
