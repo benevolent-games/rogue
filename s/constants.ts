bba265ebadd964014ea15405b23b38c3f70a1c15
Chase Moskal <chasemoskal@gmail.com>
2024-12-09 17:11:26 -0800

    enhance: csp with slip ticks

--- s/constants.ts
+++ s/constants.ts
@@ -16,7 +16,7 @@ export const constants = {
 
 	game: {
 		tickRate: 60,
-		snapshotRate: 3,
+		snapshotRate: 1,
 		cameraRotation: Degrees.toRadians(45),
 		crusaderRadius: 0.4,
 	},
--- s/logic2/flows/client.ts
+++ s/logic2/flows/client.ts
@@ -11,10 +11,10 @@ import {replicas} from "../entities/replicas.js"
 import {World} from "../../tools/babylon/world.js"
 import {RogueEntities} from "../entities/entities.js"
 import {Liaison} from "../../archimedes/net/relay/liaison.js"
+import {InputShell} from "../../archimedes/framework/parts/types.js"
 import {Chronicle} from "../../archimedes/framework/parts/chronicle.js"
 import {GameState} from "../../archimedes/framework/parts/game-state.js"
 import {Simulator} from "../../archimedes/framework/simulation/simulator.js"
-import {InputShell, Snapshot} from "../../archimedes/framework/parts/types.js"
 import {Replicator} from "../../archimedes/framework/replication/replicator.js"
 import {MultiplayerClient} from "../../archimedes/net/multiplayer/multiplayer-client.js"
 
@@ -39,13 +39,12 @@ export async function clientFlow(multiplayer: MultiplayerClient) {
 	const activeGameState = csp ? futureSimulator.gameState : baseSimulator.gameState
 	const replicator = new Replicator<RogueEntities, Realm>(author, realm, activeGameState, replicas)
 	const liaison = new Liaison(multiplayer.gameFiber)
-
 	const inputHistory = new Chronicle<InputShell<any>[]>(50)
 
 	let baseTick = 0
-	let futureTick = 0
+	let slipTick = 0
 
-	function calculateNumberOfLocalPredictionTicks() {
+	function getLatencyInTicks() {
 		const discrepancy = liaison.pingponger.averageRtt
 		return Scalar.clamp(
 			Math.round(discrepancy / (1000 / constants.game.tickRate)),
@@ -54,36 +53,19 @@ export async function clientFlow(multiplayer: MultiplayerClient) {
 		)
 	}
 
-	function repredict(tick: number, snapshot: Snapshot) {
-		const ticksToPredictAhead = calculateNumberOfLocalPredictionTicks()
-		futureTick = tick + ticksToPredictAhead
-
-		futureSimulator.gameState.restore(snapshot)
-		for (const ahead of loop(ticksToPredictAhead)) {
-			const t = tick + ahead
-			const localHistoricalInputs = inputHistory.load(t) ?? []
-			futureSimulator.simulate(t, localHistoricalInputs)
-		}
-	}
-
 	const stopTicking = interval.hz(constants.game.tickRate, () => {
 		const authoritative = liaison.take()
 
-		// gather, record, and send local inputs
-		const localInputs = replicator.gatherInputs(futureTick)
-		if (localInputs.length > 0) {
-			inputHistory.save(futureTick, localInputs)
-			liaison.sendInputs({tick: futureTick, inputs: localInputs})
-		}
-
 		// snapshots from host
 		if (authoritative.snapshot) {
+			slipTick = 0
 			baseTick = authoritative.snapshot.tick
 			baseSimulator.gameState.restore(authoritative.snapshot.data)
 		}
 
 		// inputs from host
 		else if (authoritative.inputPayloads.length > 0) {
+			slipTick = 0
 			for (const {tick, inputs} of authoritative.inputPayloads) {
 				for (let missingTick = baseTick + 1; missingTick < tick; missingTick++)
 					baseSimulator.simulate(missingTick, [])
@@ -92,9 +74,32 @@ export async function clientFlow(multiplayer: MultiplayerClient) {
 			}
 		}
 
-		// forward prediction
-		repredict(baseTick, baseSimulator.gameState.snapshot())
+		// nothing from host, but time marches forward regardless
+		else {
+			slipTick =+ 1
+		}
 
+		const ticksAhead = getLatencyInTicks() + slipTick
+		const futureTick = baseTick + ticksAhead
+
+		// gather, record, and send local inputs
+		const localInputs = replicator.gatherInputs(futureTick)
+		if (localInputs.length > 0) {
+			inputHistory.save(futureTick, localInputs)
+			liaison.sendInputs({tick: futureTick, inputs: localInputs})
+		}
+
+		// forward prediction
+		if (csp) {
+			futureSimulator.gameState.restore(baseSimulator.gameState.snapshot())
+			for (const ahead of loop(ticksAhead)) {
+				const t = baseTick + ahead
+				const localHistoricalInputs = inputHistory.load(t) ?? []
+				futureSimulator.simulate(t, localHistoricalInputs)
+			}
+		}
+
+		// replicate (3d rendering)
 		replicator.replicate(csp ? futureTick : baseTick)
 	})
 
