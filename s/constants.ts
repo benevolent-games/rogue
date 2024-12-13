57740ec3440601fd71e71c4b5c8310dae0c1d918
Chase Moskal <chasemoskal@gmail.com>
2024-12-12 19:40:04 -0800

    tweak: shader path

--- s/constants.ts
+++ s/constants.ts
@@ -9,7 +9,7 @@ export const constants = {
 		envmap: "/assets/studiolights.env",
 		templateGlb: "/assets/dungeons/byzantium.glb",
 		shaders: {
-			retro: "/assets/shaders/retro-nopix.json",
+			retro: "/assets/shaders/retro.json",
 		},
 	},
 
