9e438643388f95c74fc5318464ea591f089fb10a
Chase Moskal <chasemoskal@gmail.com>
2024-12-15 14:54:32 -0800

    upgrade: dungeons to byzantium-001

--- s/constants.ts
+++ s/constants.ts
@@ -7,7 +7,7 @@ export const constants = {
 		cover: "/assets/images/rogue-crusade-poster.webp",
 		benevLogo: "/assets/graphics/benevolent2.svg",
 		envmap: "/assets/studiolights.env",
-		templateGlb: "/assets/dungeons/byzantium.glb",
+		templateGlb: "/assets/dungeons/byzantium-001.glb",
 		shaders: {
 			retro: "/assets/shaders/retro.json",
 		},
