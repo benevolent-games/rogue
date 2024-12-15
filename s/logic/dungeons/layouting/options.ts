7a846b5a696d5889abac352818965d5344da3431
Chase Moskal <chasemoskal@gmail.com>
2024-12-15 14:44:43 -0800

    fix: use random seed

--- s/logic/dungeons/layouting/options.ts
+++ s/logic/dungeons/layouting/options.ts
@@ -4,8 +4,8 @@ import {DungeonOptions} from "./types.js"
 
 export function stdDungeonOptions(): DungeonOptions {
 	return {
-		seed: 591185246,
-		// seed: Randy.randomSeed(),
+		// seed: 591185246,
+		seed: Randy.randomSeed(),
 		gridExtents: {
 			cells: [3, 3],
 			tiles: [24, 24],
