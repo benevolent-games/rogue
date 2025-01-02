8132e5df232ee5dc353ef752d2c0cf58917db92c
Chase Moskal <chasemoskal@gmail.com>
2025-01-01 21:40:11 -0800

    add: advanced layout profiling

--- s/logic/dungeons/layout.ts
+++ s/logic/dungeons/layout.ts
@@ -1,5 +1,6 @@
 
 import {Vec2} from "@benev/toolbox"
+import {Profiler} from "../../tools/profiler.js"
 import {DungeonSpace} from "./layouting/space.js"
 import {TileStore} from "./layouting/tile-store.js"
 import {DungeonOptions} from "./layouting/types.js"
@@ -14,16 +15,29 @@ export class DungeonLayout {
 	goalposts: Vec2[]
 
 	constructor(public options: DungeonOptions) {
+		const profiler = new Profiler("dungeon layout")
+
 		const space = new DungeonSpace(options)
 		const sectors = generateSectors(space)
+		profiler.capture("generate sectors")
+
 		const {floors, goalposts} = generateFloorTiles(space, sectors)
+		profiler.capture("generate floor tiles")
 
 		const fixes = fixKissingCorners([...floors.lookup.keys()])
+		profiler.capture("fix kissing corners")
+
 		floors.insertGlobalTiles(fixes.array())
+		profiler.capture("insert fixes")
 
 		const wallTiles = inferWallTiles([...floors.tiles()])
+		profiler.capture("infer wall tiles")
+
 		const walls = new TileStore(space)
 		walls.insertGlobalTiles(wallTiles.array())
+		profiler.capture("insert walls")
+
+		profiler.report()
 
 		this.space = space
 		this.floors = floors
