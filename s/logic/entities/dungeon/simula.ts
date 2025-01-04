03cd4771245683c065d0d5ef091a6e3a91898464
Chase Moskal <chasemoskal@gmail.com>
2025-01-03 17:25:35 -0800

    wip: physics work

--- s/logic/entities/dungeon/simula.ts
+++ s/logic/entities/dungeon/simula.ts
@@ -1,7 +1,9 @@
 
+import {Vec2, Vec3} from "@benev/toolbox"
 import {RogueEntities} from "../entities.js"
 import {Station} from "../../station/station.js"
 import {Dungeon} from "../../dungeons/dungeon.js"
+import {Box2} from "../../physics/shapes/box2.js"
 import {simula} from "../../../archimedes/framework/simulation/types.js"
 
 export const dungeonSimula = simula<RogueEntities, Station>()<"dungeon">(
@@ -12,23 +14,34 @@ export const dungeonSimula = simula<RogueEntities, Station>()<"dungeon">(
 	station.dungeon = dungeon
 
 	let count = 0
+	out: for (const {sector, cell, tiles} of dungeon.layout.floors) {
+		for (const spawn of randy.take(2, tiles.array())) {
+			if (count > 10)
+				break out
+			count += 1
 
-	// out: for (const {sector, cell, tiles} of dungeon.layout.floors) {
-	// 	for (const spawn of randy.take(2, tiles.array())) {
-	// 		if (count > 10)
-	// 			break out
-	// 		count += 1
-	//
-	// 		const proposedSpawnpoint = dungeon.layout.space.toGlobalTileSpace(sector, cell, spawn)
-	// 		const availableSpawnpoint = dungeon.findNearestOpenFloorTile(proposedSpawnpoint)
-	// 		if (availableSpawnpoint) {
-	// 			simulator.create("block", {
-	// 				coordinates: availableSpawnpoint.center.array(),
-	// 				dimensions: [0.9, 0.9, 0.9],
-	// 			})
-	// 		}
-	// 	}
-	// }
+			const dimensions = new Vec3(
+				randy.range(0.2, 1.5),
+				randy.range(0.2, 1.5),
+				randy.range(0.2, 1.5),
+			)
+			const proposedSpawnpoint = dungeon.layout.space.toGlobalTileSpace(sector, cell, spawn)
+			const box = new Box2(proposedSpawnpoint, new Vec2(dimensions.x, dimensions.z))
+			const availableCenter = dungeon.findAvailableSpace(box)
+
+			console.log("SPAWN")
+			console.log(" - proposed", proposedSpawnpoint.toString())
+
+			if (availableCenter) {
+				box.center = availableCenter
+				console.log(" - available", box.center.toString())
+				simulator.create("block", {
+					coordinates: box.center.array(),
+					dimensions: dimensions.array(),
+				})
+			}
+		}
+	}
 
 	console.log("spawned blocks", count)
 
