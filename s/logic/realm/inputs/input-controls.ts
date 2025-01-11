7960ed92632a1388134b1b93c1e3f33caa9aa5cd
Chase Moskal <chasemoskal@gmail.com>
2025-01-11 15:28:00 -0800

    tweak: cam control rmb and mmb only

--- s/logic/realm/inputs/input-controls.ts
+++ s/logic/realm/inputs/input-controls.ts
@@ -11,14 +11,12 @@ export class InputControls {
 	dragQueen: DragQueen
 
 	constructor(public cameraman: Cameraman) {
-		const isLeftMouse = (event: PointerEvent) => event.button === 0
 		const isRightMouse = (event: PointerEvent) => event.button === 2
 		const isMiddleMouse = (event: PointerEvent) => event.button === 1
 		const sensitivityRadians = Arcseconds.toRadians(this.sensitivityApd)
 
 		this.dragQueen = new DragQueen({
 			predicate: event => (
-				isLeftMouse(event) ||
 				isRightMouse(event) ||
 				isMiddleMouse(event)
 			),
