9bc741e10fa3b9be03cc6ad6eac4188873e33848
Chase Moskal <chasemoskal@gmail.com>
2025-01-25 18:16:34 -0800

    add: pallet meshoids

--- s/tools/babylon/logistics/pallet.ts
+++ s/tools/babylon/logistics/pallet.ts
@@ -1,6 +1,6 @@
 
 import {Map2} from "@benev/slate"
-import {Prop} from "@benev/toolbox"
+import {Meshoid, Prop} from "@benev/toolbox"
 import {TransformNode} from "@babylonjs/core/Meshes/transformNode.js"
 import {AnimationGroup} from "@babylonjs/core/Animations/animationGroup.js"
 import {AssetContainer, InstantiatedEntries} from "@babylonjs/core/assetContainer.js"
@@ -11,13 +11,15 @@ import {Manifest} from "./manifest.js"
 import {Warehouse} from "./warehouse.js"
 import {applySpatial} from "./apply-spatial.js"
 import {PoolNoodle} from "../optimizers/pool.js"
-import {getChildProps} from "../babylon-helpers.js"
+import {getChildProps, getMeshoids} from "../babylon-helpers.js"
 
 /** an instance of an asset container, useful to maintain the brittle relationships between animationGroups and skeletons */
 export class Pallet implements PoolNoodle {
 	root: Prop
 	warehouse: Warehouse
 	instantiated: InstantiatedEntries
+
+	meshoids = new Set<Meshoid>()
 	animationGroups = new Map2<string, AnimationGroup>()
 
 	constructor(public container: AssetContainer) {
@@ -33,6 +35,10 @@ export class Pallet implements PoolNoodle {
 				.flatMap(root => [root, ...getChildProps(root).values()])
 				.map(prop => new Cargo(container.scene, prop, Manifest.scan(prop)))
 		)
+
+		for (const cargo of this.warehouse)
+			for (const [,meshoid] of getMeshoids(cargo.prop))
+				this.meshoids.add(meshoid)
 
 		for (const animationGroup of this.instantiated.animationGroups)
 			this.animationGroups.set(animationGroup.name, animationGroup)
