4783d3997929968917953ced46151fb38458b1e6
Chase Moskal <chasemoskal@gmail.com>
2025-02-09 02:09:26 -0800

    rename: exports.ts to index.ts

--- s/game/entities/replicas.ts
+++ s/game/entities/replicas.ts
@@ -1,7 +1,7 @@
 
 import {Realm} from "../realm/realm.js"
 import {RogueEntities} from "./entities.js"
-import {Replicas} from "../../packs/archimedes/exports.js"
+import {Replicas} from "../../packs/archimedes/index.js"
 
 import {botReplica} from "./bot/replica.js"
 import {blockReplica} from "./block/replica.js"
