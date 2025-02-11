15e5f2f902dc02c30352bc3bb1fa87b3c342f698
Chase Moskal <chasemoskal@gmail.com>
2025-02-11 03:02:30 -0800

    rename: kv2 to kv

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -2,7 +2,7 @@
 import "@benev/slate/x/node.js"
 
 import {Suite} from "cynic"
-import kv from "./packs/kv2/kv.test.js"
+import kv from "./packs/kv/kv.test.js"
 import buckets from "./tools/buckets/buckets.test.js"
 import archimedes from "./packs/archimedes/test.test.js"
 import inboxOutbox from "./packs/archimedes/net/relay/inbox-outbox.test.js"
